6ac0691b3a8d12e62dd96c41d6f66034aeb7a8ef
JinhaeCoder <justezy0210@gmail.com>
2021-06-07 22:51:12 +0900

    tximport

--- data/kallisto.run.sh
+++ data/kallisto.run.sh
@@ -1,3 +1,3 @@
-kallisto quant -i ./ix/Araport11_genes.201606.cdna.ix -o output --single -l 200 -s 20 ./fastq/${1}.fastq && \
+kallisto quant -i ./ix/Araport11_genes.201606.cdna.idx -o output --single -l 200 -s 20 ./fastq/${1}.fastq && \
 mv ./output/abundance.tsv ./tpm/${1}_abundance.tsv && \
 rm -rf ./output
